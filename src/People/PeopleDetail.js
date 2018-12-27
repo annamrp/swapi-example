606144fa1935d265bc2ad7145069430b08c46215
annamrp <annamrp22@gmail.com>
2018-12-27 19:13:00 +0100

    erased commented code

--- src/People/PeopleDetail.js
+++ src/People/PeopleDetail.js
@@ -67,55 +67,6 @@ export default class PeopleDetail extends Component {
       })
     }    
   } 
-
-  // searchSpecies(){
-  //   const {species} = this.props.person;
-    
-  //   if(species[0] === undefined){
-  //     this.setState({
-  //       personSpecies: {name: 'n/a', language: 'n/a'}
-  //     });
-  //   }else{
-  //     const speciesId = species[0].match(/[0-9]+/)
-  //     Swapi.getSpecies(speciesId, response => {
-  //       this.setState({
-  //         personSpecies: response,
-  //       })
-  //     })
-  //   }
-  // }
-
-  // searchFilms(){
-  //   const {films} = this.props.person;
-  //   films.forEach(film => {
-  //     let filmId = film.match(/[0-9]+/)
-  //     Swapi.getFilm(filmId, response => {
-  //       this.setState({
-  //         filmsList: this.state.filmsList.concat(response),
-  //         isLoading: false
-  //       });
-  //     });
-  //   }) 
-  // }
-
-  // searchPlanet(){
-  //   const {homeworld} = this.props.person;
-  //   const homeworldId = homeworld.match(/[0-9]+/);
-  //   Swapi.getPlanet(homeworldId, response => {
-  //     this.setState({
-  //       personHomeworld: response
-  //     })
-  //   })
-  // }
-
-  // load(){
-  //   this.searchPlanet();
-  //   this.searchFilms();
-  //   this.setState({
-  //     isLoading: true
-  //   },
-  //   )
-  // }
  
   renderFilms(){
     const {filmsList} = this.state;
